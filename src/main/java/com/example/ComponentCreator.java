62e6d7d3a7de90ee184237d65c7e9e3a14033346
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2020-04-18 22:44:58 -0300

    Almost there

--- src/main/java/com/example/ComponentCreator.java
+++ src/main/java/com/example/ComponentCreator.java
@@ -2,33 +2,31 @@ package com.example;
 
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.MustacheFactory;
-import com.intellij.lang.ASTNode;
-import com.intellij.lang.Language;
-import com.intellij.lang.ecmascript6.psi.*;
-import com.intellij.navigation.ItemPresentation;
+import com.intellij.lang.ecmascript6.psi.ES6ExportDeclaration;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.FileOutputStream;
+import java.io.Reader;
+import java.io.Writer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 
-import com.intellij.openapi.util.Key;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.*;
-import com.intellij.psi.scope.PsiScopeProcessor;
-import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.search.SearchScope;
-import com.intellij.util.IncorrectOperationException;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
 
 public class ComponentCreator extends AnAction {
     static String ERROR_TITLE = "Failed to create Component";
@@ -61,24 +59,30 @@ public class ComponentCreator extends AnAction {
 
     private void addToFile(Project project, VirtualFile virtualFile, String componentName) {
         PsiFile psiFile = PsiManager.getInstance(project).findFile(virtualFile);
-
         PsiElement[] children = psiFile.getChildren();
         PsiElement exportBefore = null;
 
-        for(int i = 0; i < children.length; i++) {
-            PsiElement el = children[i];
-
-            if(el instanceof ES6ExportDeclaration) {
+        for (PsiElement el : children) {
+            if (el instanceof ES6ExportDeclaration) {
                 ES6ExportDeclaration export = (ES6ExportDeclaration) el;
                 String exportName = export.getExportSpecifiers()[0].getDeclaredName();
 
-                if(exportBefore == null || exportName.compareTo(componentName) < -1) {
+                if (exportName.compareTo(componentName) <= 0) {
                     exportBefore = el;
                 }
             }
         }
 
-        psiFile.addAfter(exportBefore, exportBefore);
+        if(exportBefore == null && children.length > 0) {
+            exportBefore = children[children.length - 1];
+        }
+
+        PsiElement finalExportBefore = exportBefore;
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            psiFile.addAfter(ExportDeclaration.create(project, componentName), finalExportBefore);
+        });
+
+        FileDocumentManager.getInstance().saveDocument(PsiDocumentManager.getInstance(project).getDocument(psiFile));
     }
 
     @Override
@@ -95,23 +99,6 @@ public class ComponentCreator extends AnAction {
 
         if(!isValidComponentName(componentName)) {
             return;
-        }
-
-        VirtualFile parentIndexTSFile = entry.isDirectory() ? entry.findFileByRelativePath("index.ts") : entry.findFileByRelativePath("../index.ts");
-        VirtualFile parentIndexTSXFile = entry.isDirectory() ? entry.findFileByRelativePath("index.tsx") : entry.findFileByRelativePath("../index.tsx");
-
-        if(parentIndexTSFile != null) {
-            addToFile(project, parentIndexTSFile, componentName);
-        } else if(parentIndexTSXFile != null) {
-            addToFile(project, parentIndexTSXFile, componentName);
-        } else {
-            try {
-                File file = new File(Paths.get(path).resolve("index.ts").toString());
-                file.createNewFile();
-                createFile(file, componentName, "parent-export");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
 
         Path componentFolder = Paths.get(path).resolve(Paths.get(componentName));
@@ -134,21 +121,21 @@ public class ComponentCreator extends AnAction {
             Messages.showInfoMessage("Failed to create files", ComponentCreator.ERROR_TITLE);
         }
 
-//        VirtualFile parentIndexTSFile = entry.isDirectory() ? entry.findFileByRelativePath("index.ts") : entry.findFileByRelativePath("../index.ts");
-//        VirtualFile parentIndexTSXFile = entry.isDirectory() ? entry.findFileByRelativePath("index.tsx") : entry.findFileByRelativePath("../index.tsx");
-//
-//        if(parentIndexTSFile != null) {
-//            addToFile(project, parentIndexTSFile, componentName);
-//        } else if(parentIndexTSXFile != null) {
-//            addToFile(project, parentIndexTSXFile, componentName);
-//        } else {
-//            try {
-//                File file = new File(Paths.get(path).resolve("index.ts").toString());
-//                file.createNewFile();
-//                createFile(file, componentName, "parent-export");
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
+        VirtualFile parentIndexTSFile = entry.isDirectory() ? entry.findFileByRelativePath("index.ts") : entry.findFileByRelativePath("../index.ts");
+        VirtualFile parentIndexTSXFile = entry.isDirectory() ? entry.findFileByRelativePath("index.tsx") : entry.findFileByRelativePath("../index.tsx");
+
+        if(parentIndexTSFile != null) {
+            addToFile(project, parentIndexTSFile, componentName);
+        } else if(parentIndexTSXFile != null) {
+            addToFile(project, parentIndexTSXFile, componentName);
+        } else {
+            try {
+                File file = new File(Paths.get(path).resolve("index.ts").toString());
+                file.createNewFile();
+                createFile(file, componentName, "parent-export");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ src/main/java/com/example/ExportDeclaration.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import com.intellij.lang.Language;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.*;
+
+public class ExportDeclaration {
+    public static PsiElement create(Project project, String componentName) {
+        PsiFileFactory factory = PsiFileFactory.getInstance(project);
+        return factory.createFileFromText(Language.findLanguageByID("JavaScript"), (CharSequence) "export { default as " + componentName + " } from './" + componentName + "'").getFirstChild();
+    }
+}
