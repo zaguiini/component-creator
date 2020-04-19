ba6b65e56c73e7eb000463db300b22465643dcd8
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2020-04-19 14:38:31 -0300

    Enhance implementation

--- src/main/java/com/example/ComponentCreator.java
+++ src/main/java/com/example/ComponentCreator.java
@@ -1,42 +1,27 @@
 package com.example;
 
-import com.github.mustachejava.DefaultMustacheFactory;
-import com.github.mustachejava.MustacheFactory;
+import com.intellij.ide.fileTemplates.FileTemplate;
+import com.intellij.ide.fileTemplates.FileTemplateManager;
+import com.intellij.ide.fileTemplates.FileTemplateUtil;
+import com.intellij.ide.fileTemplates.actions.CreateFromTemplateActionBase;
 import com.intellij.lang.ecmascript6.psi.ES6ExportDeclaration;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.VisualPosition;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.FileOutputStream;
-import java.io.Reader;
-import java.io.Writer;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
-
-import com.intellij.openapi.vfs.LocalFileSystem;
+import java.util.Properties;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
+import com.intellij.psi.*;
 
 public class ComponentCreator extends AnAction {
     static String ERROR_TITLE = "Failed to create Component";
 
-    private String getPath(VirtualFile entry) {
-        return entry.isDirectory() ? entry.getPath() : entry.getParent().getPath();
+    private VirtualFile getPath(VirtualFile entry) {
+        return entry.isDirectory() ? entry : entry.getParent();
     }
 
     private String getComponentName(Project project) {
@@ -47,18 +32,6 @@ public class ComponentCreator extends AnAction {
     private boolean isValidComponentName(String name) {
         // TODO: Have a better matcher
         return name != null && name.matches("^[A-Za-z]+$");
-    }
-
-    private void createFile(File file, String componentName, String template) throws IOException {
-        MustacheFactory mustache = new DefaultMustacheFactory();
-        HashMap<String, Object> scopes = new HashMap<>();
-        scopes.put("componentName", componentName);
-
-        Reader reader = new InputStreamReader(getClass().getResource("/templates/" + template).openStream());
-        Writer writer = new OutputStreamWriter(new FileOutputStream(file));
-        mustache.compile(reader, template).execute(writer, scopes);
-        reader.close();
-        writer.close();
     }
 
     private void addToFile(Project project, VirtualFile virtualFile, String componentName) {
@@ -81,7 +54,7 @@ public class ComponentCreator extends AnAction {
                 }
             }
         }
-;
+
         int finalPosition = position;
         WriteCommandAction.runWriteCommandAction(project, () -> {
             PsiElement newExport = ExportDeclaration.create(project, componentName);
@@ -96,41 +69,48 @@ public class ComponentCreator extends AnAction {
         FileDocumentManager.getInstance().saveDocument(PsiDocumentManager.getInstance(project).getDocument(psiFile));
     }
 
+    private PsiElement writeFile(String template, String fileName, Project project, Properties attributes, PsiDirectory dir) throws Exception {
+        FileTemplateManager templateManager = FileTemplateManager.getInstance(project);
+        FileTemplate componentTemplate = templateManager.getInternalTemplate(template);
+        return FileTemplateUtil.createFromTemplate(componentTemplate, fileName, attributes, dir);
+    }
+
     @Override
     public void actionPerformed(AnActionEvent event) {
         Project project = event.getProject();
         VirtualFile entry = event.getData(CommonDataKeys.VIRTUAL_FILE);
 
-        if(entry == null) {
+        if(project == null || entry == null) {
             return;
         }
 
-        String path = getPath(entry);
+        VirtualFile path = getPath(entry);
         String componentName = getComponentName(project);
 
         if(!isValidComponentName(componentName)) {
             return;
         }
 
-        Path componentFolder = Paths.get(path).resolve(Paths.get(componentName));
-        File componentIndexFile = new File(componentFolder.resolve("index.ts").toString());
-        String componentFilePath = componentFolder.resolve(componentName + ".tsx").toString();
-        File componentFile = new File(componentFilePath);
+        Properties attributes = new Properties();
+        attributes.put("ComponentName", componentName);
 
-        if(!componentIndexFile.getParentFile().mkdirs()) {
-            Messages.showInfoMessage("Failed to create directory or it already exists", ComponentCreator.ERROR_TITLE);
+        PsiManager manager = PsiManager.getInstance(project);
+
+        if(path.findFileByRelativePath(componentName) != null) {
+            Messages.showInfoMessage("Directory already exists", ComponentCreator.ERROR_TITLE);
             return;
         }
 
-        try {
-            componentIndexFile.createNewFile();
-            createFile(componentIndexFile, componentName, "index");
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            manager.findDirectory(path).createSubdirectory(componentName);
+        });
 
-            componentFile.createNewFile();
-            createFile(componentFile, componentName, "component");
-        } catch (IOException e) {
+        try {
+            writeFile("ComponentIndex.ts", "index.ts", project, attributes, manager.findDirectory(entry).findSubdirectory(componentName));
+            PsiElement el = writeFile("ComponentTemplate.tsx", componentName + ".tsx", project, attributes, manager.findDirectory(entry).findSubdirectory(componentName));
+            CreateFromTemplateActionBase.startLiveTemplate(el.getContainingFile());
+        } catch (Exception e) {
             e.printStackTrace();
-            Messages.showInfoMessage("Failed to create files", ComponentCreator.ERROR_TITLE);
         }
 
         VirtualFile parentIndexTSFile = entry.isDirectory() ? entry.findFileByRelativePath("index.ts") : entry.findFileByRelativePath("../index.ts");
@@ -142,16 +122,11 @@ public class ComponentCreator extends AnAction {
             addToFile(project, parentIndexTSXFile, componentName);
         } else {
             try {
-                File file = new File(Paths.get(path).resolve("index.ts").toString());
-                file.createNewFile();
-                createFile(file, componentName, "parent-export");
-            } catch (IOException e) {
+                writeFile("ParentExport.ts", "index.ts", project, attributes, manager.findDirectory(entry).findSubdirectory(componentName));
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
-        FileEditorManager.getInstance(project).openFile(LocalFileSystem.getInstance().refreshAndFindFileByIoFile(componentFile), true);
-        Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
-        editor.getCaretModel().moveToVisualPosition(new VisualPosition(5, 11));
     }
 }
